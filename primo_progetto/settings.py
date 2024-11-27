eeccce9e79e0aa827245141b13152380bbb247ba
ferraraleonardo <149669672+ferraraleonardo@users.noreply.github.com>
2024-11-27 11:07:34 +0100

    aggiornato

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0cx(i%43b0r@#3rfa7(68b=0fmj(y9fgi4cu)=t4k5op=#vxat
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
