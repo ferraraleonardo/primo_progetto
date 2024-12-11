f66a2d91800b7906e84da6fc9e4fc85e4c14e8ca
ferraraleonardo <149669672+ferraraleonardo@users.noreply.github.com>
2024-12-11 10:22:43 +0100

    passo 10

---
+++ seconda_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def es_if(request):
+    context = {
+        'var1' : 200,
+        'var2' : 200,
+        'var3' : 300
+    }
+    return render(request, "es_if.html", context)
+
+# Create your views here.
