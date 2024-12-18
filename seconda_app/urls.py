83846175dab5ba82011d5855a1692bc136e0a2fd
ferraraleonardo <149669672+ferraraleonardo@users.noreply.github.com>
2024-12-18 11:24:17 +0100

    passo 12

--- seconda_app/urls.py
+++ seconda_app/urls.py
@@ -1,10 +1,11 @@
 from os import path
 from django.urls import path # type: ignore
-from seconda_app.views import es_if
-from seconda_app.views import index2
+from seconda_app.views import es_if, es_for, if_else_elif, index2
 
 app_name="seconda_app"
 urlpatterns=[
     path('es_if', es_if, name= 'es_if'),
+    path('if_else_elif', if_else_elif, name= 'if_else_elif'),
+    path('es_for', es_for, name= 'es_for'),
     path('index2', index2, name= 'index2'),
 ]
