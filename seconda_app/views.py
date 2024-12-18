735be241fcf3bc106eb323279b71a4d0f5cda2d7
ferraraleonardo <149669672+ferraraleonardo@users.noreply.github.com>
2024-12-18 10:30:20 +0100

    passo 12

--- seconda_app/views.py
+++ seconda_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import datetime
 
 def es_if(request):
     context = {
@@ -18,3 +19,10 @@ def if_else_elif(request):
         'var3' : 100.50,
     }
     return render(request, "if_else_elif.html", context)
+
+def es_for(request):
+    context = {
+        'list1': [1, datetime.date(2019,7,16), 'Do not give up!'],
+        'list2': [1, datetime.date(2019,7,16), 'Do not give up!']
+    }
+    return render(request, "es_for.html", context)
