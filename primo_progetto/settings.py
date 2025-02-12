c098f110ccbcc26d601c463e70a46e1aeb47ab58
Sam-Github-71 <150336617+Sam-Github-71@users.noreply.github.com>
2025-02-12 11:32:03 +0100

    verifica_corsi

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'prima_app',
     'seconda_app',
+    'news',
+    'corsi_formazione',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +61,8 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'prima_app/templates'),
                  os.path.join(BASE_DIR, 'templates'),
-                 os.path.join(BASE_DIR, 'seconda_app/templates')],
+                 os.path.join(BASE_DIR, 'seconda_app/templates'),
+                 os.path.join(BASE_DIR, 'news/templates')],
 
         'APP_DIRS': True,
         'OPTIONS': {
